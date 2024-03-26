fee3d7c717cfe6a1463a988a24283cdc4e1bd771
tmarku4 <139398597+tmarku4@users.noreply.github.com>
2024-03-26 17:38:33 -0400

    m

---
+++ public/custom-service-worker.js
@@ -0,0 +1,13 @@
+self.__WB_MANIFEST;
+
+self.addEventListener("push", function (event) {
+  const data = event.data.text();
+  const title = data || "A new message!";
+  const options = {
+    body: data.body || "You have a new notification.",
+    icon: "/icons/icon-192x192.png",
+    badge: "/icons/badge-72x72.png",
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
